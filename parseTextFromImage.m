edd3778af475ff6f8f5878d60a46342038ae2fde
cleverclue <cleverclue@gmail.com>
2016-05-22 13:59:54 -0400

    Add files via upload

--- parseTextFromImage.m
+++ parseTextFromImage.m
@@ -39,9 +39,49 @@ plot(sortSize(1:length(sortSize)-1),'.')
 maskPiece=(sizePiece>50)&(sizePiece<5000);
 ahypoth=sum(maskPiece);
 selectPieces=find(maskPiece);
-[rankPieces,indexPieces]=sort(sizePiece(selectPieces));
-hypPuzzlePieces=bythenumber==-255;
+[orderPieces,indexPieces]=sort(sizePiece(selectPieces));
 rankPieces=selectPieces(indexPieces);
 figure(6)
-plot(rankPieces,'.')
+plot(orderPieces,'.')
 
+%%
+numInfo=(zeros(ahypoth,1));
+sortedPuzzle=uint8(zeros(size(puzzle1)));
+for i=1:ahypoth
+    j=rankPieces(i);
+    %boxPiece=uint16(a(j).BoundingBox);
+    boxRangeX=boxPiece(j,2)-1 + (0:boxPiece(j,4));
+    boxRangeY=boxPiece(j,1)-1 + (0:boxPiece(j,3));
+    %imshow(puzzle1(boxRangeX,boxRangeY,:))
+    %pause(1)
+    numInfo(i)=sum(sum(puzzle1(boxRangeX,boxRangeY,1)<30));
+    if numInfo(i)>10
+        sortedPuzzle(boxRangeX,boxRangeY,:)=puzzle1(boxRangeX,boxRangeY,:);
+    end
+end
+%%
+
+
+[dum,indexInfo]=sort(numInfo,'descend');
+lowInfo=find(dum<10);
+limitInfo=lowInfo(01);
+getPieces=boxPiece(rankPieces(indexInfo(1:limitInfo)),:);
+listPiecesByInfo=uint8(zeros(max(getPieces(:,4)),limitInfo+sum(getPieces(:,3)),3));
+
+offsetY=0;
+for i=1:limitInfo
+    boxRangeX=getPieces(i,2)-1 + (0:getPieces(i,4));
+    boxRangeY=getPieces(i,1)-1 + (0:getPieces(i,3));
+    listRangeX=1+(0:getPieces(i,4));
+    listRangeY=1+(0:getPieces(i,3))+offsetY;
+    totalPixels=(1+getPieces(i,4))*(1+getPieces(i,3));
+    %imshow(puzzle1(boxRangeX,boxRangeY,:))
+    %title(int2str(i))
+    %pause(0.1)
+    listPiecesByInfo(listRangeX,listRangeY,:)=puzzle1(boxRangeX,boxRangeY,:);
+    offsetY=offsetY+getPieces(i,3)+1;
+end
+
+%%
+figure(7)
+imshow(listPiecesByInfo)
